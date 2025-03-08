e5cd55173555bb803f73b503e2a73d659736416f
Ben Young <44130260+mrpleiades@users.noreply.github.com>
2025-03-07 23:45:48 -0500

    Updated Run Elevator PID values

--- src/main/java/frc/robot/commands/RunElevatorPos.java
+++ src/main/java/frc/robot/commands/RunElevatorPos.java
@@ -1,13 +1,16 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.elevator.Elevator;
 
 public class RunElevatorPos extends Command {
   private final Elevator elevator;
   private final double elevatorPos;
-  private final PIDController elevatorController = new PIDController(1, 0, 0);
+  // private final PIDController elevatorController = new PIDController(1, 0, 0);
+  private final ProfiledPIDController elevatorController =
+      new ProfiledPIDController(0.05, 0.001, 0, new TrapezoidProfile.Constraints(10, 1));
 
   public RunElevatorPos(Elevator elevator, double elevatorPos) {
     this.elevator = elevator;
@@ -17,12 +20,13 @@ public class RunElevatorPos extends Command {
 
   @Override
   public void initialize() {
-    elevatorController.setSetpoint(elevatorPos);
+    // elevatorController.setGoal(elevatorPos);
+    // elevatorController.setSetpoint(elevatorPos);
   }
 
   @Override
   public void execute() {
-    double calc = elevatorController.calculate(elevator.getTotalPos());
+    double calc = elevatorController.calculate(elevator.getTotalPos(), elevatorPos);
     elevator.runFirst(calc);
     elevator.runSecond(calc);
   }
