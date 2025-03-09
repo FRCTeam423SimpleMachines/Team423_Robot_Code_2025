2f2d9859c7cfcaa1011844f46bf1c72fc6b36cd2
GL-321 <99373370+GL-321@users.noreply.github.com>
2025-03-09 14:48:56 -0400

    Changed Drive motors to brake mode and increased MaxAmbiguity

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -58,7 +58,7 @@ public class VisionConstants {
           new Rotation3d(0.0, 0.0, -3.0 * Math.PI / 4.0));
 
   // Basic filtering thresholds
-  public static double maxAmbiguity = 0.1;
+  public static double maxAmbiguity = 0.15;
   public static double maxZError = 0.75;
 
   // Standard deviation baselines, for 1 meter distance and 1 tag
